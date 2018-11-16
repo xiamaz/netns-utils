0bda7d6b2d6b817481f19fbdc8614cd8bd1907c2
Max Zhao <alcasa.mz@gmail.com>
2018-11-16 10:19:01 +0100

    Fixing main script by using alternative iptables commands

--- main.sh
+++ main.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -e
 
-usage="# $0 <add|del> namespace_name"
+usage="# $0 <add|del> namespace_name internet_dev"
+
+guess_network() {
+	ip link | grep 'state UP' | awk '{print substr($2, 1, length($2)-1)}'
+}
 
 printerr() {
 	>&2 echo "$@"
@@ -11,8 +15,7 @@ printerr() {
 create_netns() {
 	printerr "# Create network namespace"
 	ip netns add "$1"
-	ip netns exec "$1" ip addr add 127.0.0.1/8 dev lo
-	ip netns exec "$1" ip link set lo up
+	ip netns exec "$1" ip link set dev lo up
 	# add dns routing inside the nameserver
 	mkdir -p /etc/netns/"$1"
 	echo 'nameserver 1.1.1.1' > /etc/netns/"$1"/resolv.conf
@@ -43,8 +46,8 @@ create_interfaces() {
 	subnet="$2"
 	printerr "Outer interface $vpn0 and inner $vpn1 associated with $subnet."
 	ip link add "$vpn0" type veth peer name "$vpn1"
-	ip link set "$vpn0" up
 	ip link set "$vpn1" netns "$1" up
+	ip link set "$vpn0" up
 	ip addr add "$subnet.1/24" dev "$vpn0"
 	ip netns exec "$1" ip addr add "$subnet.2/24" dev "$vpn1"
 	ip netns exec "$1" ip route add default via "$subnet.1" dev "$vpn1"
@@ -55,33 +58,43 @@ create_interfaces() {
 
 # 1 - name of entrypoint to veth
 # 2 - subnet of veth
+# 3 - name of external network device
 enable_routing() {
-	printerr "# Create iptables rules."
-	if ! iptables -C INPUT \! -i "$1" -s "${2}.0/24" -j DROP 2> /dev/null; then
-		printerr "INPUT rule created for ${2}.0/24"
-		iptables -A INPUT \! -i "$1" -s "${2}.0/24" -j DROP
-	else
-		printerr "INPUT rule already exists for ${2}.0/24"
-	fi
-	if ! iptables -t nat -C POSTROUTING -s "${2}.0/24" -o wl+ -j MASQUERADE 2> /dev/null; then
-		printerr "POSTROUTING rule created for ${2}.0/24 wl+"
-		iptables -t nat -A POSTROUTING -s "${2}.0/24" -o wl+ -j MASQUERADE
-	else
-		printerr "POSTROUTING rule already exists for ${2}.0/24 wl+"
-	fi
-	if ! iptables -t nat -C POSTROUTING -s "${2}.0/24" -o en+ -j MASQUERADE 2> /dev/null; then
+	printerr "# Create iptables forward between $1 $3"
+	iptables -A FORWARD -i $1 -o $3 -j ACCEPT
+	iptables -A FORWARD -o $1 -i $3 -j ACCEPT
+	if ! iptables -t nat -C POSTROUTING -s "${2}.0/24" -o $3 -j MASQUERADE 2> /dev/null; then
 		printerr "POSTROUTING rule created for ${2}.0/24 en+"
-		iptables -t nat -A POSTROUTING -s "${2}.0/24" -o en+ -j MASQUERADE
+		iptables -t nat -A POSTROUTING -s "${2}.0/24" -o $3 -j MASQUERADE
 	else
 		printerr "POSTROUTING rule already exists for ${2}.0/24 en+"
 	fi
 	sysctl -q net.ipv4.ip_forward=1
 }
 
+# 1 - name of veth entrypoint
+# 2 - name of external network device
+disable_routing() {
+	iptables -D FORWARD -i $1 -o $2 -j ACCEPT
+	iptables -D FORWARD -o $1 -i $2 -j ACCEPT
+}
+
 if [ $(id -u) -ne 0 ]; then
 	echo "Please run as root."
 	echo $usage
 	exit
+fi
+
+if [ $# -lt 2 ]; then
+	echo "$usage"
+	exit
+fi
+
+if [ -z $3 ]; then
+	extnet=$(guess_network)
+	echo "Guessing network interface to be at: $extnet"
+else
+	extnet=$3
 fi
 
 case "$1" in
@@ -93,7 +106,7 @@ case "$1" in
 		create_netns "$2"
 		subnet=$(get_free_subnet)
 		vethext=$(create_interfaces "$2" "$subnet")
-		enable_routing "$vethext" "$subnet"
+		enable_routing "$vethext" "$subnet" "$extnet"
 		;;
 	del)
 		if ! ip netns | grep "$2" > /dev/null; then
@@ -101,10 +114,11 @@ case "$1" in
 			exit
 		fi
 		printerr "Delete netns for $2"
-		sudo ip netns del "$2"
-		printerr "Delete ${2}veth0 link"
-		sudo ip link del "${2}veth0"
-		printerr "iptables not cleaned. Check manually."
+		ip netns del "$2"
+		vethext="${2}veth0"
+		printerr "Delete $vethext link"
+		ip link del "$vethext"
+		disable_routing $vethext $extnet
 		;;
 	"")
 		echo $usage
